841eb831bfef834b1117a9e1806d751bb46f1983
Henrique0440 <59101905+Henrique0440@users.noreply.github.com>
2023-08-09 11:11:07 -0300

    Add files via upload

---
+++ src/Produtos_JS/p_c9.js
@@ -0,0 +1,19 @@
+const c9 = [
+    {
+        nome: "Revitalizador C9",
+        imagem: "./Image/Produtos/C9/C9-REVITALIZADOR.png",
+        endereco: "./product-page/revitalizadorc9.html",
+    }, 
+    {
+        nome: "Rev. Porcelanato C9",
+        imagem: "./Image/Produtos/C9/porcelanatoc9.png",
+        endereco: "./product-page/porcelanatoc9.html",
+    },
+    {
+        nome: "Flotador C9",
+        imagem: "./Image/Produtos/C9/flotadorc9.png",
+        endereco: "./product-page/flotadorc9.html",
+    },
+]
+
+export { c9 }
--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import { autolimpe } from "./Produtos_JS/p_autolimpe.js";
 import { klimp } from "./Produtos_JS/p_klimp.js";
 import { start } from "./Produtos_JS/p_start.js";
+import { c9 } from "./Produtos_JS/p_c9.js";
 
 let p_start = document.getElementById('p_start')
 let p_autolimpe = document.getElementById('p_autolimpe')
 let p_klimp = document.getElementById('p_klimp')
+let p_c9 = document.getElementById('p_c9')
 
 
 document.addEventListener('DOMContentLoaded', function () {
     console.table(start)
     console.table(autolimpe)
     console.table(klimp)
+    console.table(c9)
 
     start.map((val) => {
         p_start.innerHTML += `
@@ -55,6 +58,22 @@ document.addEventListener('DOMContentLoaded', function () {
             infop[i].addEventListener("click", function () {
                 if (!klimp[i].endereco || klimp[i].endereco == './product-page/') return window.alert("Pagina não disponivel, tente novamente mais tarde!")
                 location.href = klimp[i].endereco
+            })
+        }
+    })
+
+    c9.map((val) => {
+        p_c9.innerHTML += `
+        <div class="formprod">
+        <img class="btn_c9" src="`+ val.imagem + `">
+        <h1>`+ val.nome + `</h1>
+        </div>`
+
+        var infop = document.getElementsByClassName("btn_c9")
+        for (let i = 0; i < infop.length; i++) {
+            infop[i].addEventListener("click", function () {
+                if (!c9[i].endereco || c9[i].endereco == './product-page/') return window.alert("Pagina não disponivel, tente novamente mais tarde!")
+                location.href = c9[i].endereco
             })
         }
     })
